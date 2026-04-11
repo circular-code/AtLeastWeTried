6db0950a00499bb28b8088ab0233131ab6af03c2
test <le.sch@live.de>
2026-04-11 14:21:12 +0200

    Fix ui

--- webui/src/renderer/shaders/scannerCone.js
+++
@@ -1,3 +0,0 @@
-import SCANNER_CONE_VERTEX_SHADER from './scannerCone.vert.glsl?raw';
-import SCANNER_CONE_FRAGMENT_SHADER from './scannerCone.frag.glsl?raw';
-export { SCANNER_CONE_VERTEX_SHADER, SCANNER_CONE_FRAGMENT_SHADER };
--- webui/src/renderer/shaders/unitBody.js
+++
@@ -1,3 +0,0 @@
-import UNIT_BODY_VERTEX_SHADER from './unitBody.vert.glsl?raw';
-import UNIT_BODY_FRAGMENT_SHADER from './unitBody.frag.glsl?raw';
-export { UNIT_BODY_VERTEX_SHADER, UNIT_BODY_FRAGMENT_SHADER };
